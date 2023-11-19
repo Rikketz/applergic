dca157f2e2765e1a7aab05f40d41b62bd3ce3fcf
misspaz <macarenapaz91@gmail.com>
2023-11-19 12:56:18 +0100

    login annd register ok

--- src/api/controllers/users.controllers.js
+++ src/api/controllers/users.controllers.js
@@ -1,74 +1,71 @@
-const cloudinary = require('cloudinary').v2;  // Importa la biblioteca Cloudinary
-const bcrypt = require('bcrypt');
-const { validateEmailDB } = require('../../utils/validator');
-const { generarToken } = require('../../utils/jwt');
-const User = require('../models/user.models');
+const cloudinary = require("cloudinary").v2;
+const bcrypt = require("bcrypt");
+const { validateEmailDB } = require("../../utils/validator");
+const { generarToken } = require("../../utils/jwt");
+const User = require("../models/user.models");
 
+const registerEmergencyContact = async (req, res) => {
+  const { userId } = req.params;
+  const updateFields = req.body;
 
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: { contactoEmergencia: updateFields } },
+      { new: true }
+    );
 
-// const register = async (req, res) => {
-//   try {
-//     const { email, password, nombreCompleto, direccion, telefono } = req.body;
+    if (!updatedUser) {
+      return res.status(404).json({ message: "Este usuario no existe" });
+    }
 
-//     if (!email || !password || !nombreCompleto || !direccion || !telefono) {
-//       return res.status(400).json({ success: false, message: "Todos los campos son requeridos" });
-//     }
-
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     if (!emailRegex.test(email)) {
-//       return res.status(400).json({ success: false, message: "El formato del correo electrónico no es válido" });
-//     }
-
-//     if (password.length < 6) {
-//       return res.status(400).json({ success: false, message: "La contraseña debe tener al menos 6 caracteres" });
-//     }
-
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     const userBody = new User({
-//       email,
-//       password: hashedPassword,
-//       nombreCompleto,
-//       direccion,
-//       telefono,
-//     });
-
-//     const createdUser = await userBody.save();
-
-//     return res.status(201).json({ success: true, message: "Usuario registrado con éxito", data: createdUser });
-//   } catch (error) {
-//     console.error('Error en el registro:', error);
-//     return res.status(500).json({ success: false, message: "Error interno del servidor", error: error.message });
-//   }
-// };
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error(
+      "Error en la actualización del contacto de emergencia:",
+      error
+    );
+    return res.status(500).json({
+      success: false,
+      message: "Error interno del servidor",
+      error: error.message,
+    });
+  }
+};
 
 const register = async (req, res) => {
   try {
     const { email, password, nombreCompleto, direccion, telefono } = req.body;
 
-    // Verificar si se proporciona una foto como archivo
-    let fotoUrl = '';
+    let fotoUrl = "";
     if (req.file) {
       const result = await cloudinary.uploader.upload(req.file.path);
       fotoUrl = result.secure_url;
     }
 
-    // Verificar si se proporciona una foto como URL
     if (req.body.fotoUrl) {
       fotoUrl = req.body.fotoUrl;
     }
 
     if (!email || !password || !nombreCompleto || !direccion || !telefono) {
-      return res.status(400).json({ success: false, message: "Todos los campos son requeridos" });
+      return res
+        .status(400)
+        .json({ success: false, message: "Todos los campos son requeridos" });
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-      return res.status(400).json({ success: false, message: "El formato del correo electrónico no es válido" });
+      return res.status(400).json({
+        success: false,
+        message: "El formato del correo electrónico no es válido",
+      });
     }
 
     if (password.length < 6) {
-      return res.status(400).json({ success: false, message: "La contraseña debe tener al menos 6 caracteres" });
+      return res.status(400).json({
+        success: false,
+        message: "La contraseña debe tener al menos 6 caracteres",
+      });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -79,36 +76,54 @@ const register = async (req, res) => {
       nombreCompleto,
       direccion,
       telefono,
-      foto: fotoUrl,  // Guardar la URL de la foto
+      foto: fotoUrl,
     });
 
     const createdUser = await userBody.save();
 
-    return res.status(201).json({ success: true, message: "Usuario registrado con éxito", data: createdUser });
+    const userId = createdUser._id;
+
+    return res.status(201).json({
+      success: true,
+      message: "Usuario registrado con éxito",
+      data: createdUser,
+    });
   } catch (error) {
-    console.error('Error en el registro:', error);
-    return res.status(500).json({ success: false, message: "Error interno del servidor", error: error.message });
+    console.error("Error en el registro:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Error interno del servidor",
+      error: error.message,
+    });
   }
 };
 
 const login = async (req, res) => {
-    try {
-        const userInfo = req.body;
-        const userDB = await validateEmailDB(userInfo.email);
-        if(!userDB){ 
-            return res.json({succes: false, message : "Email no existe"})
-        }
-        if(!bycrypt.compareSync(userInfo.password, userDB.password)){
-            return res.json({succes: false, message : "La contraseña no coincide"})
-        }
-
-        const token = generarToken(userDB._id, userDB.email);
-        return res.json({succes: true, message : "Esta ok", token: token, userInfo: userDB})
-    } catch (error) {
-        
+  try {
+    const userInfo = req.body;
+    const userDB = await validateEmailDB(userInfo.email);
+    if (!userDB) {
+      return res.json({ succes: false, message: "Email no existe" });
     }
-}
+    if (!bcrypt.compareSync(userInfo.password, userDB.password)) {
+      return res.json({ succes: false, message: "La contraseña no coincide" });
+    }
 
+    const token = generarToken(userDB._id, userDB.email);
+    return res.json({
+      succes: true,
+      message: "Esta ok",
+      token: token,
+      userInfo: userDB,
+    });
+  } catch (error) {
+    console.error("Error en el login:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Error interno del servidor",
+      error: error.message,
+    });
+  }
+};
 
-
-module.exports = {register, login}
+module.exports = { register, login, registerEmergencyContact };
--- src/api/routes/users.routes.js
+++ src/api/routes/users.routes.js
@@ -1,11 +1,14 @@
 
 const express = require("express");
-const {register, login, profile} = require("../controllers/users.controllers");
+const {register, login, profile, registerEmergencyContact, updateUser} = require("../controllers/users.controllers");
 const router = express.Router()
 const upload = require("../Middleware/upload.file")
-
+const usersController = require('../controllers/users.controllers');
 
 router.post("/register", upload.single('foto'), register);
 router.post("/login" , login);
+router.put('/register-emergency-contact/:userId', registerEmergencyContact);
 
 module.exports = router;
+
+
